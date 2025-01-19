040d9caff31d69cee0a887b869165971eb3437db
Cart1416 <cbella1416@gmail.com>
2025-01-19 18:05:06 -0500

    Update getchromium.sh

--- getchromium.sh
+++ getchromium.sh
@@ -1,1 +1,2 @@
+mkdir -p chromium
 wget -O chromium/chromium.AppImage https://github.com/ivan-hc/Chromium-Web-Browser-appimage/releases/download/continuous/Chromium-stable-132.0.6834.83-x86_64.AppImage
