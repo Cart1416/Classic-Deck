7a5e71bc100413feb08a516fe9fb48eed15b1263
Cart1416 <83319239+Cart1416@users.noreply.github.com>
2025-01-24 17:14:40 -0500

    Update installminecraft.sh

--- scripts/minecraft/installminecraft.sh
+++ scripts/minecraft/installminecraft.sh
@@ -1,3 +1,3 @@
 flatpak install flathub io.mrarm.mcpelauncher
 
-$1 --add-to-steam "Minecraft Bedrock Edition" "flatpak run io.mrarm.mcpelauncher"
+$1 --add-to-steam "Minecraft Bedrock Edition" "flatpak" "run io.mrarm.mcpelauncher"
